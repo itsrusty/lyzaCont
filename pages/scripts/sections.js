8fb4ba06e5ec5bc872768e94f05f847f2c516bc4
itsrusty <rusty3031@gmail.com>
2023-04-22 19:19:14 -0700

    add modals

--- pages/scripts/sections.js
+++ pages/scripts/sections.js
@@ -1,4 +1,29 @@
-const clients = document.querySelector(".text-clients")
-clients.addEventListener("click", ()=>{
-    window.location.assign("./clients.html")
-})
+// clientes
+const clients = document.querySelector(".text-clients");
+clients.addEventListener("click", () => {
+  window.location.assign("./clients.html");
+});
+
+// facturas
+const facturas = document.querySelector(".text-facturas");
+facturas.addEventListener("click", () => {
+  window.location.assign("./facturas.html");
+});
+
+// gastos recibidos
+const gastosRecibidos = document.querySelector(".text-pagos");
+gastosRecibidos.addEventListener("click", () => {
+  window.location.assign("./pagos.recibidos.html");
+});
+
+// providers
+const providers = document.querySelector(".text-providers");
+providers.addEventListener("click", () => {
+  window.location.assign("./providers.html");
+});
+
+// gastos realizados
+const gastosRealizados = document.querySelector(".text-pagosRealizados");
+gastosRealizados.addEventListener("click", () => {
+  window.location.assign("./pagos.realizados.html");
+});
