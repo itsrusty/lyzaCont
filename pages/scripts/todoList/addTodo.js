193762d5da2ed7c4fb03c4161eaa5f8dae8b5b7d
itsrusty <rusty3031@gmail.com>
2023-04-24 17:23:51 -0700

    add pages

--- pages/scripts/todoList/addTodo.js
+++ pages/scripts/todoList/addTodo.js
@@ -1,4 +1,14 @@
-const buttonNewTask = document.querySelector(".button")
-buttonNewTask.addEventListener("click",()=>{
-    
-})
+const buttonNewTask = document.querySelector(".addTask");
+buttonNewTask.addEventListener("click", () => {
+  const task = document.querySelector(".taskContent");
+  const todoListContent = document.querySelector(".todo-list");
+  const data = document.createElement("li");
+  console.log(`message: ${task.value}`);
+
+
+  // render data
+  data.innerText = task.value;
+  todoListContent.appendChild(data);
+
+  task.value = "";
+});
